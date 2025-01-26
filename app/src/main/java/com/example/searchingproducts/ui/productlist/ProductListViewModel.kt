34e0f0c68d308f0198f1ca15aa90814ca6c9b4b3
Julian Carvajal <nettoingenieria@gmail.com>
2025-01-26 18:19:33 -0500

    Handle errors implemented

--- app/src/main/java/com/example/searchingproducts/ui/productlist/ProductListViewModel.kt
+++ app/src/main/java/com/example/searchingproducts/ui/productlist/ProductListViewModel.kt
@@ -4,9 +4,9 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.searchingproducts.data.remote.model.Paging
 import com.example.searchingproducts.data.remote.model.SearchResponse
 import com.example.searchingproducts.data.repository.ProductRepository
+import com.example.searchingproducts.ui.UiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
@@ -19,23 +19,41 @@ class ProductListViewModel @Inject constructor(
     private val _searchResults = MutableLiveData<SearchResponse>()
     val searchResults: LiveData<SearchResponse> = _searchResults
 
+    private val _uiState = MutableLiveData<UiState<SearchResponse>>()
+    val uiState : LiveData<UiState<SearchResponse>> = _uiState
+
+    private var lastQuery: String? = null
+    private var lastCategoryId: String? = null
+
     fun searchProducts(query: String, categoryId: String?){
+
+        lastQuery = query
+        lastCategoryId = categoryId
+
+        executeSearch(query, categoryId)
+    }
+
+    private fun executeSearch(query: String, categoryId: String?) {
         viewModelScope.launch {
+            _uiState.value = UiState.Loading
             try {
                 val response = if (categoryId != null) {
                     repository.searchByCategory(categoryId)
                 } else {
                     repository.searchProducts(query)
                 }
-                _searchResults.value = response
+                _uiState.value = UiState.Success(response)
             } catch (e: Exception){
-                _searchResults.value = SearchResponse(
-                    siteId = "",
-                    query = "",
-                    paging = Paging(0,0,0,0),
-                    results = emptyList()
+                _uiState.value = UiState.Error(
+                    e.message ?: "Error inesperado"
                 )
             }
+        }
+    }
+
+    fun retry() {
+        lastQuery?.let { query ->
+            executeSearch(query, lastCategoryId)
         }
     }
 }
