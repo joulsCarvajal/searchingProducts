93abe92ff95a3ddc123fd4e54549d54e27522579
Julian Carvajal <nettoingenieria@gmail.com>
2025-01-27 16:13:26 -0500

    added some UI and orientation modifications. I have also added the latest search

--- app/src/main/java/com/example/searchingproducts/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/example/searchingproducts/ui/search/SearchViewModel.kt
@@ -14,7 +14,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
-    private val repository: ProductRepository
+    private val repository: ProductRepository,
+    private val searchPreferences: SearchPreferences
 ) : ViewModel() {
 
     private val _searchResults = MutableLiveData<SearchResponse>()
@@ -23,17 +24,28 @@ class SearchViewModel @Inject constructor(
     private val _uiState = MutableLiveData<UiState<SearchResponse>>()
     val uiState: LiveData<UiState<SearchResponse>> = _uiState
 
+    private val _recentSearches = MutableLiveData<List<String>>()
+    val recentSearches : LiveData<List<String>> = _recentSearches
+
+    init {
+        loadRecentSearches()
+    }
+
     private var lastQuery: String? = null
 
     fun searchProducts(query: String) {
         lastQuery = query
+        // Guardar la búsqueda reciente
+        searchPreferences.saveRecentSearch(query)
+        // Recargar las búsquedas recientes
+        loadRecentSearches()
+
         viewModelScope.launch {
             _uiState.value = UiState.Loading
             try {
                 val response = repository.searchProducts(query)
-                Log.d("SearchViewModel", "Products: ${response.results.size}")
-                Log.d("SearchViewModel", "First product: ${response.results.firstOrNull()}")
                 _searchResults.value = response
+                _uiState.value = UiState.Success(response)  // Asegúrate de actualizar el estado
             } catch (e: Exception) {
                 _uiState.value = UiState.Error(e.message ?: "Error inesperado")
                 Log.e("SearchViewModel", "Error: ${e.message}")
@@ -44,5 +56,9 @@ class SearchViewModel @Inject constructor(
     //Permite restaurar la última búsqueda
     fun retryLastSearch() {
         lastQuery?.let { searchProducts(it) }
+    }
+
+    private fun loadRecentSearches() {
+        _recentSearches.value = searchPreferences.getRecentSearches()
     }
 }
